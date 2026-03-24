531248e03ca78d0856145ba8d85a4e769cffad6f
Andres Elizalde <andep210399@gmail.com>
2026-03-24 07:26:22 -0700

    feat: :sparkles: informacion en bruto

---
+++ material-ejemplo/src/data/navigation.js
@@ -0,0 +1,12 @@
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import SchoolRoundedIcon from "@mui/icons-material/SchoolRounded";
+import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+
+// Ítems de navegación lateral y bottom navigation
+export const NAV_ITEMS = [
+  { label: "Inicio", icon: <HomeRoundedIcon /> },
+  { label: "Cursos", icon: <SchoolRoundedIcon /> },
+  { label: "Calendario", icon: <CalendarMonthRoundedIcon /> },
+  { label: "Perfil", icon: <PersonRoundedIcon /> },
+];
